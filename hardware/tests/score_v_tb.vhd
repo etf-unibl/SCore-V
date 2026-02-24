ca615ad6ed84b924289bedc5fcf3383d2ad43e72
thewantedboss <ana.vulin@student.etf.unibl.org>
2026-02-24 14:23:47 +0100

    Issue #71 : Extend risc-v with arithmetic instructions - Added entity design for risc-v - Added testbench design to verify results

--- hardware/tests/score_v_tb.vhd
+++ hardware/tests/score_v_tb.vhd
@@ -112,7 +112,8 @@ architecture sim of score_v_tb is
     7  => (28, "0010011", "000", "0000000", 3, 1, 0, 12, '1'),
     8  => (32, "0000011", "010", "0000000", 2, 0, 0,  0, '1'),
     9  => (36, "0000011", "010", "0000000", 1, 0, 0,  8, '1'),
-    10 => (40, "0100011", "010", "0000000", 0, 0, 1, 25, '0')
+    10 => (40, "0100011", "010", "0000000", 0, 0, 1, 25, '0'),
+    11 => (44, "0110011", "000", "0100000", 16, 4, 5, -2, '1')
   );
 
 begin
@@ -172,7 +173,7 @@ begin
         wait until rising_edge(clk_s);
         rst_s <= '0';
 
-        for i in 0 to 11 loop
+        for i in 0 to 12 loop
           wait until rising_edge(clk_s);
           full_instr := instr_mem_s.other_instruction_bits & instr_mem_s.opcode;
 
