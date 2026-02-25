1099f58622ffc790db5c12b7860c5ec263ec266b
natasamiljevic <natasamiljevic@users.noreply.github.com>
2026-02-25 21:18:26 +0000

    deploy: ec1cc518590a1a6b8d2ba08df7529e3f4604a9ea

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['comb_5fproc_0',['comb_proc',['../classcontrol_1_1arch.html#aa1842b4ac950c2d03361a017a3d43c1b',1,'control::arch']]]
+  ['comb_5fproc_0',['comb_proc',['../classcontrol_1_1arch.html#a5a7e0de76c7373e3ef749fd425764653',1,'control::arch']]]
 ];
