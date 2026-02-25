f2d6834b3307223ec3d68bedda4055906e6b61d7
natasamiljevic <natasamiljevic@users.noreply.github.com>
2026-02-25 19:42:40 +0000

    deploy: 7c887a3e59ad5e19b0e89641bb22114572c9a755

--- search/variables_f.js
+++ search/variables_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['t_5falu_5fop_0',['t_alu_op',['../classalu__pkg.html#a400fb7cfd6b766411d269ec6530adc86',1,'alu_pkg']]],
+  ['t_5falu_5fop_0',['t_alu_op',['../classalu__pkg.html#a76b719c1be2d1a517adaef52546ffeca',1,'alu_pkg']]],
   ['t_5fbyte_1',['t_byte',['../classmem__pkg.html#a035297a06be6a2436d6945dbc434cd9e',1,'mem_pkg']]],
   ['t_5fbytes_2',['t_bytes',['../classmem__pkg.html#a80d20e2384c52749579a21659c8b9250',1,'mem_pkg']]],
   ['t_5finstruction_5frec_3',['t_instruction_rec',['../classmem__pkg.html#a3347bc436dedb097d5c7e8fd57707c3a',1,'mem_pkg']]],
