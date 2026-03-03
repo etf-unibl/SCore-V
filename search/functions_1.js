e7aa1977b14ff38d8eb9fcc5e083832c9908a4c9
thewantedboss <thewantedboss@users.noreply.github.com>
2026-03-03 16:16:08 +0000

    deploy: b5619d352b8008f6c986c8a25b459ebc05d96c20

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['imm_5fproc_0',['imm_proc',['../classimm__gen_1_1arch.html#a48e3c33ebbb50f79429876b6ca31683b',1,'imm_gen::arch']]]
+  ['imm_5fproc_0',['imm_proc',['../classimm__gen_1_1arch.html#a4e4d582114fffbff44f0b3cf16cbb204',1,'imm_gen::arch']]]
 ];
