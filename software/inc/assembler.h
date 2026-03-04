d8b4427722eafa2fcf189e39321540cbe8f91c6e
Luka Zeljko <luka@lukazeljko.xyz>
2026-03-04 11:05:21 +0100

    Issue #131 : Create assembly to machine code converter
    - Added alu functions needed for result generator
    - Added initial result generator functions
    - Added warnings flag to Makefile

--- software/inc/assembler.h
+++ software/inc/assembler.h
@@ -9,36 +9,60 @@ typedef enum {
     S_TYPE
 } InstrFormat;
 
+int add(int a, int b) { return a+b; }
+int sub(int a, int b) { return a-b; }
+int xor_op(int a, int b) { return a^b; }
+int or_op(int a, int b) { return a|b; }
+int and_op(int a, int b) { return a&b; }
+int sll(int a, int b) { return a<<b; }
+unsigned int srl(unsigned int a, unsigned int b) { return a>>b; }
+int sra(int a, int b) { return a>>b; }
+int slt(int a, int b) { return (a < b)?1:0; }
+unsigned int sltu(unsigned int a, unsigned int b) { return (a < b)?1:0; }
+
+// L and S functions below need fixing
+int lb(int a, int b) { return a+b; }
+int lh(int a, int b) { return a+b; }
+int lw(int a, int b) { return a+b; }
+int lbu(int a, int b) { return a+b; }
+int lhu(int a, int b) { return a+b; }
+
+int sb(int a, int b) { return a+b; }
+int sh(int a, int b) { return a+b; }
+int sw(int a, int b) { return a+b; }
+
 typedef struct {
     const char *name;
     InstrFormat format;
     uint8_t opcode;
     uint8_t funct3;
     uint8_t funct7;
-	uint16_t imm;
+	uint8_t signess;
+	int (*signed_operation)(int, int);
+    unsigned int (*unsigned_operation)(unsigned int, unsigned int);
 } Instruction;
 
 Instruction instr_table[] = {
-	{"add",   R_TYPE, 0x33, 0x0, 0x00},
-	{"sub",   R_TYPE, 0x33, 0x0, 0x02},
-	{"xor",   R_TYPE, 0x33, 0x4, 0x00},
-	{"or",    R_TYPE, 0x33, 0x6, 0x00},
-	{"and",   R_TYPE, 0x33, 0x7, 0x00},
-	{"sll",   R_TYPE, 0x33, 0x1, 0x00},
-	{"srl",   R_TYPE, 0x33, 0x5, 0x00},
-	{"sra",   R_TYPE, 0x33, 0x5, 0x02},
-	{"slt",   R_TYPE, 0x33, 0x2, 0x00},
-	{"sltu",  R_TYPE, 0x33, 0x3, 0x00},
+	{"add",   R_TYPE, 0x33, 0x0, 0x00, 0, add, NULL},
+	{"sub",   R_TYPE, 0x33, 0x0, 0x02, 0, sub, NULL},
+	{"xor",   R_TYPE, 0x33, 0x4, 0x00, 0, xor_op, NULL},
+	{"or",    R_TYPE, 0x33, 0x6, 0x00, 0, or_op, NULL},
+	{"and",   R_TYPE, 0x33, 0x7, 0x00, 0, and_op, NULL},
+	{"sll",   R_TYPE, 0x33, 0x1, 0x00, 0, sll, NULL},
+	{"srl",   R_TYPE, 0x33, 0x5, 0x00, 1, NULL, srl},
+	{"sra",   R_TYPE, 0x33, 0x5, 0x02, 0, sra, NULL},
+	{"slt",   R_TYPE, 0x33, 0x2, 0x00, 0, slt, NULL},
+	{"sltu",  R_TYPE, 0x33, 0x3, 0x00, 1, NULL, sltu},
 
-	{"addi",  I_TYPE, 0x13, 0x0, 0x00},
-	{"xori",  I_TYPE, 0x13, 0x4, 0x00},
-	{"ori",   I_TYPE, 0x13, 0x6, 0x00},
-	{"andi",  I_TYPE, 0x13, 0x7, 0x00},
-	{"slli",  I_TYPE, 0x13, 0x1, 0x00},
-	{"srli",  I_TYPE, 0x13, 0x5, 0x00},
-	{"srai",  I_TYPE, 0x13, 0x5, 0x00},
-	{"slti",  I_TYPE, 0x13, 0x2, 0x00},
-	{"sltiu", I_TYPE, 0x13, 0x2, 0x00},
+	{"addi",  I_TYPE, 0x13, 0x0, 0x00, 0, add, NULL},
+	{"xori",  I_TYPE, 0x13, 0x4, 0x00, 0, xor_op, NULL},
+	{"ori",   I_TYPE, 0x13, 0x6, 0x00, 0, or_op, NULL},
+	{"andi",  I_TYPE, 0x13, 0x7, 0x00, 0, and_op, NULL},
+	{"slli",  I_TYPE, 0x13, 0x1, 0x00, 0, sll, NULL},
+	{"srli",  I_TYPE, 0x13, 0x5, 0x00, 0, NULL, srl},
+	{"srai",  I_TYPE, 0x13, 0x5, 0x00, 0, sra, NULL},
+	{"slti",  I_TYPE, 0x13, 0x2, 0x00, 0, slt, NULL},
+	{"sltiu", I_TYPE, 0x13, 0x2, 0x00, 0, NULL, sltu},
 
 	{"lb",    I_TYPE, 0x03, 0x0, 0x00},
 	{"lh",    I_TYPE, 0x03, 0x1, 0x00},
@@ -51,6 +75,8 @@ Instruction instr_table[] = {
 	{"sw",   S_TYPE, 0x23, 0x2, 0x00}
 };
 
+unsigned int pc = 0;
+
 /** Function that call process_line function on each line
  * of input .txt.
  */
@@ -59,7 +85,7 @@ void process_file(FILE *fptr);
 /** Function that find type of instruction (R, I or S) and
  * converts it to machine code approprietely.
  */
-void process_line(char line[256], FILE* fout);
+void process_line(char line[256], FILE* fout, FILE* expected_out);
 
 /** Returns register number from a string,
  * used in handle_x_type functions.
@@ -94,14 +120,18 @@ Instruction* find_instruction(const char *name);
 /** Handles R_TYPE instructions, fills uint32_t result and
  *  calls output_result function.
  */
-void handle_r_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint8_t reg2, FILE* output);
+void handle_r_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint8_t reg2, FILE* output, FILE* expected_out);
 
 /** Handles I_TYPE instructions, fills uint32_t result and
  *  calls output_result function.
  */
-void handle_i_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint16_t imm, FILE* output);
+void handle_i_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint16_t imm, FILE* output, FILE* expected_out);
 
 /** Handles S_TYPE instructions, fills uint32_t result and
  *  calls output_result function.
  */
-void handle_s_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint16_t imm, FILE* output);
+void handle_s_type(Instruction* instr, uint8_t regd, uint8_t reg1, uint16_t imm, FILE* output, FILE* expected_out);
+
+/** Prints expected output
+ */
+void output_expected(Instruction *instr, uint8_t regd, uint8_t reg1, uint8_t reg2, uint16_t imm, FILE* expected_out);
