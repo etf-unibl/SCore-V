078fbf55e1ffd114f476b165e52fd0c2045e3790
natasamiljevic <natasamiljevic@users.noreply.github.com>
2026-03-05 09:49:42 +0000

    deploy: b10b14dd5ae9795d1d240bb42557552a01b088a4

--- search/variables_c.js
+++ search/variables_c.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['pc_0',['pc',['../classscore__v_1_1arch.html#ac5320993e7995e865996ad9f239e2cfa',1,'score_v::arch']]],
-  ['pc_5fi_1',['pc_i',['../classpc__next__instruction.html#a502f81044ff8162c618e699013f2ee15',1,'pc_next_instruction']]],
-  ['pc_5fnext_5fi_2',['pc_next_i',['../classpc.html#a21cbcb54a218b71b0f435f1b195c484d',1,'pc']]],
-  ['pc_5fnext_5finstruction_3',['pc_next_instruction',['../classscore__v_1_1arch.html#a60820212aa8ec9b4b36deea93d4b4556',1,'score_v::arch']]],
-  ['pc_5fnext_5fo_4',['pc_next_o',['../classpc__next__instruction.html#a47c1c29745d97b2d7ff3958d3af1f856',1,'pc_next_instruction']]],
-  ['pc_5fnext_5fsig_5',['pc_next_sig',['../classscore__v_1_1arch.html#a8cad814ff0e9a3c737ef1ad317d71094',1,'score_v::arch']]],
-  ['pc_5fo_6',['pc_o',['../classpc.html#a3b1dd06064836b982a39318569cd4bad',1,'pc.pc_o()'],['../classscore__v.html#a3b1dd06064836b982a39318569cd4bad',1,'score_v.pc_o()']]],
-  ['pc_5freg_7',['pc_reg',['../classpc_1_1arch.html#ab1848495c0d58b151137f7d3fc1bdc30',1,'pc::arch']]],
-  ['pc_5fsel_5fi_8',['pc_sel_i',['../classpc__next__instruction.html#aa19344a11453bf7faf8cd80d8ca90561',1,'pc_next_instruction']]],
-  ['pc_5fsel_5fo_9',['pc_sel_o',['../classcontrol.html#a468809803a3c89055cc157e8f9a12136',1,'control']]],
-  ['pc_5fsel_5fsig_10',['pc_sel_sig',['../classscore__v_1_1arch.html#ad31e9fbef45c1df6b49d859b4d6f50a0',1,'score_v::arch']]],
-  ['pc_5fsig_11',['pc_sig',['../classscore__v_1_1arch.html#a328284cd6754d7c3b42f0f3cfbb5d9a9',1,'score_v::arch']]],
-  ['pc_5ftarget_5fi_12',['pc_target_i',['../classpc__next__instruction.html#ad35cf69c0f58274c16c41be9f6ca0e9f',1,'pc_next_instruction']]]
+  ['pc4_5fi_1',['pc4_i',['../classwb__mux.html#a4ffb278df1f730744f0d45d4feeb2c15',1,'wb_mux']]],
+  ['pc_5fi_2',['pc_i',['../classpc__next__instruction.html#a502f81044ff8162c618e699013f2ee15',1,'pc_next_instruction']]],
+  ['pc_5fnext_5fi_3',['pc_next_i',['../classpc.html#a21cbcb54a218b71b0f435f1b195c484d',1,'pc']]],
+  ['pc_5fnext_5finstruction_4',['pc_next_instruction',['../classscore__v_1_1arch.html#a60820212aa8ec9b4b36deea93d4b4556',1,'score_v::arch']]],
+  ['pc_5fnext_5fo_5',['pc_next_o',['../classpc__next__instruction.html#a47c1c29745d97b2d7ff3958d3af1f856',1,'pc_next_instruction']]],
+  ['pc_5fnext_5fsig_6',['pc_next_sig',['../classscore__v_1_1arch.html#a8cad814ff0e9a3c737ef1ad317d71094',1,'score_v::arch']]],
+  ['pc_5fo_7',['pc_o',['../classpc.html#a3b1dd06064836b982a39318569cd4bad',1,'pc.pc_o()'],['../classscore__v.html#a3b1dd06064836b982a39318569cd4bad',1,'score_v.pc_o()']]],
+  ['pc_5freg_8',['pc_reg',['../classpc_1_1arch.html#ab1848495c0d58b151137f7d3fc1bdc30',1,'pc::arch']]],
+  ['pc_5fsel_5fi_9',['pc_sel_i',['../classpc__next__instruction.html#aa19344a11453bf7faf8cd80d8ca90561',1,'pc_next_instruction']]],
+  ['pc_5fsel_5fo_10',['pc_sel_o',['../classcontrol.html#a468809803a3c89055cc157e8f9a12136',1,'control']]],
+  ['pc_5fsel_5fsig_11',['pc_sel_sig',['../classscore__v_1_1arch.html#ad31e9fbef45c1df6b49d859b4d6f50a0',1,'score_v::arch']]],
+  ['pc_5fsig_12',['pc_sig',['../classscore__v_1_1arch.html#a328284cd6754d7c3b42f0f3cfbb5d9a9',1,'score_v::arch']]],
+  ['pc_5ftarget_5fi_13',['pc_target_i',['../classpc__next__instruction.html#ad35cf69c0f58274c16c41be9f6ca0e9f',1,'pc_next_instruction']]]
 ];
