afceb1d50a8e278c183f6663147bb5ee04d4b636
Bosko Brankovic <bosko.brankovic@student.etf.unibl.org>
2026-03-05 12:20:08 +0100

    Issue #109 : Modify memory package for IMEM initialization - Fetch instruction changed - Testbench for instruction fetch changed - Memory package modified - Function for parsing data implemented - Directory for initialization files added

--- hardware/tests/fetch_instruction_tb.vhd
+++ hardware/tests/fetch_instruction_tb.vhd
@@ -35,7 +35,7 @@
 -- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 -- OTHER DEALINGS IN THE SOFTWARE
 -----------------------------------------------------------------------------
-library vunit_lib;  
+library vunit_lib;
 context vunit_lib.vunit_context;
 library design_lib;
 
@@ -55,6 +55,7 @@ architecture arch of fetch_instruction_tb is
 
   signal test_in  : std_logic_vector(31 downto 0);
   signal test_out : t_instruction_rec;
+  signal c_IMEM   : t_bytes := initialize_memory("instruction_memory.txt");
 
 begin
   --! @brief UUT instantiation and port mapping.
@@ -63,18 +64,17 @@ begin
     instruction_count_i => test_in,
     instruction_bits_o  => test_out
     );
-  
   main : process
     variable full_instruction : std_logic_vector(31 downto 0);
     variable addr_int         : integer;
 
   begin
     test_runner_setup(runner, runner_cfg);
-    
+
     while test_suite loop
       if run("test_fetch_instruction") then
         for i in 0 to 16 loop
-          if i mod 4 = 0 then 
+          if i mod 4 = 0 then
             test_in <= std_logic_vector(to_unsigned(i, 32));
             wait for 100 ns;
           end if;
@@ -84,10 +84,10 @@ begin
                         c_IMEM(addr_int + 1) &
                         c_IMEM(addr_int);
         end loop;
-        check_equal(test_out.opcode, full_instruction(6 downto 0), 
+        check_equal(test_out.opcode, full_instruction(6 downto 0),
                     "Opcode mismatch at index " & integer'image(addr_int));
 
-        check_equal(test_out.other_instruction_bits, full_instruction(31 downto 7), 
+        check_equal(test_out.other_instruction_bits, full_instruction(31 downto 7),
                     "Data bits mismatch at index " & integer'image(addr_int));
       end if;
     end loop;
