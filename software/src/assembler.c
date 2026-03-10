5acfcfaaa1e2c7074111168fa78980b5f8642ee4
Luka Zeljko <luka@lukazeljko.xyz>
2026-03-10 08:51:30 +0100

    Issue #131 : Create assembly to machine code converter
    - Assembler will now load initial dmem from data_memory.txt file

--- software/src/assembler.c
+++ software/src/assembler.c
@@ -19,6 +19,7 @@ int main(int argc, char* argv[]) {
 
 void process_file(FILE* fptr) {
 	char line[256];
+	char line_dmem[5];
 	FILE* fout;
 	fout = fopen("../hardware/init_files/instruction_memory.txt", "w");
 	if(fout == NULL) {
@@ -33,6 +34,21 @@ void process_file(FILE* fptr) {
 		return;
 	}
 
+	// Load DMEM
+	FILE *fdmem;
+	fdmem = fopen("../hardware/init_files/data_memory.txt", "r");
+	if(fdmem == NULL) {
+		printf("Unable to load data_memory file, dmem will be empty");
+		return;
+	}
+	else {
+		int i=0;
+		while(fgets(line_dmem, sizeof(line_dmem), fdmem)) {
+			dmem[i++] = (uint8_t)strtol(line_dmem, NULL, 16);
+		}
+
+		fclose(fdmem);
+	}
 
 	while(fgets(line, sizeof(line), fptr)) {
 		process_line(line, fout, expected_out);
