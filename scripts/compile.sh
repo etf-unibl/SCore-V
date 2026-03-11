f49163a0dcdcedd4ba9e683bbc7e2345863bc449
Luka Zeljko <luka@lukazeljko.xyz>
2026-03-11 17:43:20 +0100

    Issue #131 : Create assembly to machine code converter
    - Trying to fix compile.sh not running on server

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-cd ../software/
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR/../software/"
 make assembler
 if [ $? -ne 0 ]; then
     echo "Make failed, falling back to old assembler executable"
 else
-	echo "Make successful"
+    echo "Make successful"
 fi
 ./obj/assembler ../hardware/init_files/input_program.txt
 if [ $? -ne 0 ]; then
     echo "There were errors in syntax of assembler"
     exit 1
 else
-	echo "Assembler created output files successfuly"
+    echo "Assembler created output files successfuly"
 fi
